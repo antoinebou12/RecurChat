2b9cf3dc54d1bcef5e6444f704bb4d248024f595
Antoine Boucher <antoine.bou13@gmail.com>
2023-06-17 16:36:18 -0400

    Update Room.js

--- app/database/schemas/Room.js
+++ app/database/schemas/Room.js
@@ -4,9 +4,12 @@ const Schema = mongoose.Schema;
 
 let RoomSchema = new Schema({
   title: { type: String, required: true },
-  members: { type: [{ userId: String, socketId: String }]},
+  description: { type: String, default: '' },
+  members: { type: [{ userId: String, socketId: String }], default: [] },
+  owner: { type: String, required: true },
+  messages: { type: [String], default: [] }, // assuming messages are stored in a separate collection and their IDs are stored here.
+  active: { type: Boolean, default: true },
   createdDate: { type: Date, default: Date.now },
-  
 });
 
 let roomModel = mongoose.model('room', RoomSchema);
