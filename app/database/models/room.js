8b418cb858073282ae1d0ff9f30fe88289459597
Antoine Boucher <antoine.bou13@gmail.com>
2023-06-17 16:38:16 -0400

    Update room.js

--- app/database/models/room.js
+++ app/database/models/room.js
@@ -1,40 +1,47 @@
-let roomModel = require('../schemas/Room');
-let User = require('../models/user');
+const roomModel = require('../schemas/Room');
 
-let create = function (data, callback) {
-	let newRoom = new roomModel(data);
-	newRoom.save(callback);
+const create = async (data) => {
+    if (!data.title) {
+        throw new Error('Missing required field: title');
+    }
+
+    const newRoom = new roomModel(data);
+    return await newRoom.save();
 };
 
-let find = function (data, callback) {
-	roomModel.find(data, callback);
+const find = async (data) => {
+    return await roomModel.find(data);
 };
 
-let findOne = function (data, callback) {
-	roomModel.findOne(data, callback);
+const findOne = async (data) => {
+    return await roomModel.findOne(data);
 };
 
-let findById = function (id, callback) {
-	roomModel.findById(id, callback);
+const findById = async (id) => {
+    return await roomModel.findById(id);
 };
 
-var findByIdAndUpdate = function(id, data, callback){
-	roomModel.findByIdAndUpdate(id, data, { new: true }, callback);
+const findByIdAndUpdate = async (id, data) => {
+    return await roomModel.findByIdAndUpdate(id, data, { new: true });
 };
 
-var addUser = function (room, user, socket, callback) {
+const addUser = async (room, user, socket) => {
+    // Check if user and room is valid
+    if (!user._id || !room._id) {
+        throw new Error('Invalid user or room');
+    }
 
-	// Push a new connection object(i.e. {userId + socketId})
-	var conn = { 'userId': user._id, 'socketId': socket.id};
-	room.connections.push(conn);
-	room.save(callback);
+    // Push a new connection object(i.e. {userId + socketId})
+    const conn = { 'userId': user._id, 'socketId': socket.id };
+    room.connections.push(conn);
+    return await room.save();
 };
 
 module.exports = {
-	create,
-	find,
-	findOne,
-	findById,
-	findByIdAndUpdate,
-	addUser
+    create,
+    find,
+    findOne,
+    findById,
+    findByIdAndUpdate,
+    addUser
 };
