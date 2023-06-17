78a023235dff5e0f0e0cc175682354926cd0667f
Antoine Boucher <antoine.bou13@gmail.com>
2023-06-17 16:36:47 -0400

    Update Message.js

--- app/database/schemas/Message.js
+++ app/database/schemas/Message.js
@@ -3,9 +3,12 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 let MessageSchema = new Schema({
-  data: {type: String, required: true},
+  sender: { type: Schema.Types.ObjectId, ref: 'User', required: true },
+  roomId: { type: Schema.Types.ObjectId, ref: 'Room', required: true },
+  data: { type: String, required: true },
+  type: { type: String, default: 'text' }, // 'text', 'image', 'video', etc.
   createdDate: { type: Date, default: Date.now },
-
+  editedAt: { type: Date }
 });
 
 let messageModel = mongoose.model('message', MessageSchema);
