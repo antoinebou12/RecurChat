317909b59bab3e05b8fb790368ad9265ad8a5510
Antoine Boucher <antoine.bou13@gmail.com>
2023-06-17 16:35:46 -0400

    Update User.js

--- app/database/schemas/User.js
+++ app/database/schemas/User.js
@@ -1,12 +1,16 @@
 const mongoose = require('mongoose');
-
 const Schema = mongoose.Schema;
 
 const UserSchema = new Schema({
-  username: { type: String, required: true},
+  username: { type: String, required: true },
+  email: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
+  profilePicture: { type: String, default: null },
+  bio: { type: String, default: '' },
   createdDate: { type: Date, default: Date.now },
-  permission: {type: Number, default: 1},
-
+  lastLogin: { type: Date, default: Date.now },
+  active: { type: Boolean, default: true },
+  permission: { type: Number, default: 1 },
 });
 
 let userModel = mongoose.model('user', UserSchema);
